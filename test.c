efc85b69cd360ae18a8d7d3f5b024aac24855c4f
demons <837940593@qq.com>
2017-05-27 00:54:13 +0000

    complete test.c with NIDS macro

--- test.c
+++ test.c
@@ -1,12 +1,12 @@
 #include "syntax_tree.h"
 
 #define GetLast2(N, Code) Code
-#define GetLast3(N, ...) GetLast2(__VA__ARGS__)
-#define GetLast4(N, ...) GetLast3(__VA__ARGS__)
-#define GetLast5(N, ...) GetLast4(__VA__ARGS__)
-#define GetLast6(N, ...) GetLast5(__VA__ARGS__)
-#define GetLast7(N, ...) GetLast6(__VA__ARGS__)
-#define GetLast8(N, ...) GetLast7(__VA__ARGS__)
+#define GetLast3(N, ...) GetLast2(__VA_ARGS__)
+#define GetLast4(N, ...) GetLast3(__VA_ARGS__)
+#define GetLast5(N, ...) GetLast4(__VA_ARGS__)
+#define GetLast6(N, ...) GetLast5(__VA_ARGS__)
+#define GetLast7(N, ...) GetLast6(__VA_ARGS__)
+#define GetLast8(N, ...) GetLast7(__VA_ARGS__)
 
 #define GetMacro( \
 	_1, _2, _3, \
@@ -15,42 +15,49 @@
 	...) _9
 
 #define FE1(Action, Code, N, NoU) Action(Code, N)
-#define FE2(Action, Code, N, ...) Action(Code, N) FE1(Action, Code, __VA__ARGS__)
-#define FE3(Action, Code, N, ...) Action(Code, N) FE2(Action, Code, __VA__ARGS__)
-#define FE4(Action, Code, N, ...) Action(Code, N) FE3(Action, Code, __VA__ARGS__)
-#define FE5(Action, Code, N, ...) Action(Code, N) FE4(Action, Code, __VA__ARGS__)
-#define FE6(Action, Code, N, ...) Action(Code, N) FE5(Action, Code, __VA__ARGS__)
-#define FE7(Action, Code, N, ...) Action(Code, N) FE6(Action, Code, __VA__ARGS__)
+#define FE2(Action, Code, N, ...) Action(Code, N) FE1(Action, Code, __VA_ARGS__)
+#define FE3(Action, Code, N, ...) Action(Code, N) FE2(Action, Code, __VA_ARGS__)
+#define FE4(Action, Code, N, ...) Action(Code, N) FE3(Action, Code, __VA_ARGS__)
+#define FE5(Action, Code, N, ...) Action(Code, N) FE4(Action, Code, __VA_ARGS__)
+#define FE6(Action, Code, N, ...) Action(Code, N) FE5(Action, Code, __VA_ARGS__)
+#define FE7(Action, Code, N, ...) Action(Code, N) FE6(Action, Code, __VA_ARGS__)
 
 #define ForEach(Action, N, ...) \
-    GetMacro(N, __VA__ARGS__, \
+    GetMacro(N, __VA_ARGS__, \
 	    FE7, FE6, FE5, \
 	    FE4, FE3, FE2, \
 	    FE1) \
 	(Action, \
-	GetMacro(N, __VA__ARGS__, \
+	GetMacro(N, __VA_ARGS__, \
 	    GetLast8, GetLast7, GetLast6, \
 	    GetLast5, GetLast4, GetLast4, \
-	    GetLast3, GetLast2)(N, __VA__ARGS__), \
-	N, __VA__ARGS__)
+	    GetLast3, GetLast2)(N, __VA_ARGS__), \
+	N, __VA_ARGS__)
 
 #define NID(N) \
-    void pro##N##IAction(AST_node *parent, AST_node *child, int childNum)
-    /* { \ */
-	/* #define proNum N */
+    void pro##N##IAction(AST_node *parent, AST_node *child, int childNum) \
+    { \
+    const int proNum = N;
+
+#define BEGIN \
+	;
 
 #define END \
-    #undef proNum \
-    }
+	)
 
-#define NIDSHelper(N, Code) \
-    NID(N) \
-    Code \
-    END
+#define NIDSHelper(Code, N) \
+	NID(N) \
+	Code \
+	END
 
 #define NIDS(N, ...) \
-    ForEach(NIDSHelper, N, __VA__ARGS__)
+	ForEach(NIDSHelper, N, __VA_ARGS__)
 
-/* NIDS(1, 2, 3, "Hello") */
-/* GetLast4(1, 2, 3, "Hello") */
-GetLast3(1, 2, 3)
+NIDS(1, 2, 3,
+{
+    "Hello";
+}
+;
+"Hello";
+return 0;
+)
