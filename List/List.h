9872569d3798703a8545cd06e6fbfa6b85585d0a
demons <837940593@qq.com>
2017-05-28 03:57:09 +0000

    add list

--- List/List.h
+++ List/List.h
@@ -33,4 +33,90 @@ List *newList(
     return list;
 }
 
+void append(List *list, void *data)
+{
+    ListNode *node = (ListNode *)malloc(sizeof(ListNode));
+    node->prev = list->end;
+    node->next = NULL;
+    node->data = data;
+    list->end->next = node;
+    list->end = node;
+}
+
+void cancat(List *l1, List *l2)
+{
+    l1->end->next = l2->begin;
+    l2->begin->prev = l1->end;
+    l1->end = l2->end;
+}
+
+void *getFirst(List *list)
+{
+    return list->begin->data;
+}
+
+void *getLast(List *list)
+{
+    return list->end->data;
+}
+
+void *rest(List *list, bool destory)
+{
+    if (list->begin == list->end)
+    {
+	if (destory)
+	{
+	    free(list->begin->data);
+	}
+	free(list->begin);
+	list->begin = list->end = NULL;
+    }
+    else
+    {
+	ListNode *next = list->begin->next;
+	next->prev = NULL;
+	if (destory)
+	{
+	    free(list->begin->data);
+	}
+	free(list->begin);
+	list->begin = next;
+    }
+}
+
+void *findByKey(List *list, void *key)
+{
+    ListNode *it = list->begin;
+    while (it != NULL)
+    {
+	if (list->find_f(it->data, key))
+	{
+	    return it->data;
+	}
+    }
+    return NULL;
+}
+
+bool findByData(List *list, void *data)
+{
+    ListNode *it = list->begin;
+    while (it != NULL)
+    {
+	if (list->cmp_f(it->data, data))
+	{
+	    return true;
+	}
+    }
+    return false;
+}
+
+void traversal(List *list, void (*action)(void *data))
+{
+    ListNode *it = list->begin;
+    while (it != NULL)
+    {
+	action(it->data);
+    }
+}
+
 #endif
